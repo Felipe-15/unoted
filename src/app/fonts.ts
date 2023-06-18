78cfe474dcaae31a68c6a0b9a93fe67decf3cdd7
Felipe Souza <felipesouza036@gmail.com>
2023-06-18 11:47:33 -0300

    Initial Configs

---
+++ src/app/fonts.ts
@@ -0,0 +1,10 @@
+import { Mukta, Open_Sans } from "next/font/google";
+
+export const mukta = Mukta({
+  subsets: ["latin"],
+  weight: ["600", "700"],
+});
+
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+});
