54903e9659ab9e1c0d5f3587b719e3afe587dd8c
Felipe Souza <felipesouza036@gmail.com>
2023-06-18 13:30:20 -0300

    Configuring colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          500: "#FF9000",
+          400: "#FFA733",
+        },
+        secondary: {
+          500: "#FDFFFC",
+        },
+        background: {
+          900: "#0E181B",
+          800: "#152428",
+          700: "#1C3035",
+          600: "#233C43",
+        },
+        gray: {
+          500: "#808080",
+          400: "#8C9294",
+          300: "#D6D3D3",
+        },
+        danger: "#F61313",
       },
     },
   },
   plugins: [],
-}
+};
