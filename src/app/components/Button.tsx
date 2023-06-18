9d21244ab0e2351993c31dbc106af99a2ba9370e
Felipe Souza <felipesouza036@gmail.com>
2023-06-18 16:55:31 -0300

    feat: Button component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { IconType } from "react-icons";
+
+interface Props {
+  text: string;
+  icon?: IconType;
+  onClick: () => void;
+  outline?: boolean;
+}
+
+const Button = ({ text, outline, icon: Icon, onClick }: Props) => {
+  return (
+    <button
+      className={`flex gap-2 ${
+        outline
+          ? "border-[1px] text-primary-500 border-primary-500 hover:bg-primary-500 hover:text-secondary-500"
+          : "bg-primary-500 text-secondary-500 hover:bg-primary-400"
+      } items-center justify-center w-full py-2 max-h-[40px] transition rounded-sm  text-lg`}
+      onClick={onClick}
+    >
+      {Icon && <Icon />}
+      {text}
+    </button>
+  );
+};
+
+export default Button;
