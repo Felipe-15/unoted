bda680e2e38d4826fe47cf2552868cff9f45c2c0
Felipe Souza <felipesouza036@gmail.com>
2023-07-13 22:58:00 -0300

    feat: Using env to get firebase info

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,22 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBx9ZSJHWd84ICVVSKCqrPlRCqTwjLZYSc",
-  authDomain: "unoted-749a2.firebaseapp.com",
-  projectId: "unoted-749a2",
-  storageBucket: "unoted-749a2.appspot.com",
-  messagingSenderId: "706769647523",
-  appId: "1:706769647523:web:8caf0644df8ba4709ccc16",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
