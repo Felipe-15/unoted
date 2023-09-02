1f918fce7b15c2dfee165acc7d44d0c52a5c79bd
Felipe Souza <felipesouza036@gmail.com>
2023-09-02 01:37:54 -0300

    refactor (Categories): Separate conditional interface

--- src/app/(home)/categories/page.tsx
+++ src/app/(home)/categories/page.tsx
@@ -20,6 +20,37 @@ const CategoriesPage = () => {
     handleUpdateCategory,
   } = usePage();
 
+  const noteCategories = isLoading
+    ? Array(3)
+        .fill(0)
+        .map((_, index) => <SkeletonCategory key={index.toString()} />)
+    : filteredCategories.map((category: ICategory, index) => {
+        if (category.type === "task") return;
+        return (
+          <Category
+            {...category}
+            key={category.id}
+            onUpdate={(data) => handleUpdateCategory(data, category.id)}
+            onDelete={() => handleDeleteCategory(category.id, category.type)}
+          />
+        );
+      });
+  const taskCategories = isLoading
+    ? Array(3)
+        .fill(0)
+        .map((_, index) => <SkeletonCategory key={index.toString()} />)
+    : filteredCategories.map((category: ICategory, index) => {
+        if (category.type === "note") return;
+        return (
+          <Category
+            {...category}
+            key={category.id}
+            onUpdate={(data) => handleUpdateCategory(data, category.id)}
+            onDelete={() => handleDeleteCategory(category.id, category.type)}
+          />
+        );
+      });
+
   return (
     <StandardPage user={user} onSearch={handleSearch}>
       <h2 className="pt-4 text-3xl md:text-4xl text-secondary-500 mb-4">
@@ -27,42 +58,14 @@ const CategoriesPage = () => {
       </h2>
       <div className="flex overflow-x-auto p-4 items-center gap-8 mb-4 mx-6 overflow-y-hidden">
         <AddCategoryButton onClick={() => handleAddCategory("note")} />
-        {isLoading &&
-          Array(3)
-            .fill(0)
-            .map((_, index) => <SkeletonCategory key={index.toString()} />)}
-        {filteredCategories.map((category: ICategory, index) => {
-          if (category.type === "task") return;
-          return (
-            <Category
-              {...category}
-              key={category.id}
-              onUpdate={(data) => handleUpdateCategory(data, category.id)}
-              onDelete={() => handleDeleteCategory(category.id, category.type)}
-            />
-          );
-        })}
+        {noteCategories}
       </div>
       <h2 className="pt-4 text-3xl md:text-4xl text-secondary-500 mb-4">
         Tarefas
       </h2>
       <div className="flex overflow-x-auto p-4 mx-6 items-center gap-8 overflow-y-hidden">
         <AddCategoryButton onClick={() => handleAddCategory("task")} />
-        {isLoading &&
-          Array(3)
-            .fill(0)
-            .map((_, index) => <SkeletonCategory key={index.toString()} />)}
-        {filteredCategories.map((category: ICategory, index) => {
-          if (category.type === "note") return;
-          return (
-            <Category
-              {...category}
-              key={category.id}
-              onUpdate={(data) => handleUpdateCategory(data, category.id)}
-              onDelete={() => handleDeleteCategory(category.id, category.type)}
-            />
-          );
-        })}
+        {taskCategories}
       </div>
     </StandardPage>
   );
