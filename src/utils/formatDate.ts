fe4c7b38c30e8d5f53da5f410369ea0722d389dd
Felipe Souza <felipesouza036@gmail.com>
2024-01-10 17:43:44 -0300

    refactor (formatDate): Better logic on date transformation

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -7,7 +7,9 @@ export const formatDate = (timestamp: number) => {
     year: "numeric",
   });
 
-  let replaced: any = stringData.replace(/(de\s)|\./g, "").split(" ");
+  let replaced: string | string[] = stringData
+    .replace(/(de\s)|\./g, "")
+    .split(" ");
 
   replaced[1] = replaced[1].slice(0, 1).toUpperCase() + replaced[1].slice(1);
 
