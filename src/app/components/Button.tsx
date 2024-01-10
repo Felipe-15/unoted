120024b70e326d4f12244e4c0b8eeac40a80e6e4
Felipe Souza <felipesouza036@gmail.com>
2024-01-10 17:42:02 -0300

    feat (Button): Transform in a server side component

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { HTMLAttributes } from "react";
 import { IconType } from "react-icons";
 import Loader from "./Loader";
@@ -26,7 +25,7 @@ const Button = ({
       {...rest}
       disabled={disabled}
       data-outline={outline}
-      className={`flex gap-2 data-[outline=true]:border-[1px] data-[outline=true]:text-primary-500 data-[outline=true]:border-primary-500 data-[outline=true]:hover:bg-primary-500 data-[outline=true]:hover:text-secondary-500 bg-primary-500 text-secondary-500 hover:bg-primary-400 items-center justify-center w-full py-2 max-h-[40px] data-[outline=true]:bg-transparent transition rounded-md text-lg disabled:brightness-90 disabled:hover:brightness-90 disabled:hover:bg-primary-500 data-[outline=true]:disabled:hover:bg-transparent data-[outline=true]:disabled:brightness-90 data-[outline=true]:disabled:hover:text-primary-500`}
+      className={`flex gap-2 data-[outline=true]:border-[1px] data-[outline=true]:text-primary-500 data-[outline=true]:border-primary-500 data-[outline=true]:hover:bg-primary-500 data-[outline=true]:hover:text-secondary-500 bg-primary-500 text-secondary-500 hover:bg-primary-400 items-center justify-center w-full py-2 max-h-[40px] data-[outline=true]:bg-transparent transition rounded-md text-lg disabled:brightness-90 disabled:hover:brightness-90 disabled:hover:bg-primary-500 data-[outline=true]:disabled:hover:bg-transparent data-[outline=true]:disabled:brightness-90 data-[outline=true]:disabled:hover:text-primary-500 disabled:cursor-not-allowed`}
       onClick={onClick}
     >
       {Icon && !loading && <Icon />}
