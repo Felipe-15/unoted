f6dcf3e2f09a462350144ca910a53d1632ef0076
Felipe Souza <felipesouza036@gmail.com>
2024-01-11 13:23:04 -0300

    refactor (Button): Less css to disabled styles

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -25,7 +25,7 @@ const Button = ({
       {...rest}
       disabled={disabled}
       data-outline={outline}
-      className={`flex gap-2 data-[outline=true]:border-[1px] data-[outline=true]:text-primary-500 data-[outline=true]:border-primary-500 data-[outline=true]:hover:bg-primary-500 data-[outline=true]:hover:text-secondary-500 bg-primary-500 text-secondary-500 hover:bg-primary-400 items-center justify-center w-full py-2 max-h-[40px] data-[outline=true]:bg-transparent transition rounded-md text-lg disabled:brightness-90 disabled:hover:brightness-90 disabled:hover:bg-primary-500 data-[outline=true]:disabled:hover:bg-transparent data-[outline=true]:disabled:brightness-90 data-[outline=true]:disabled:hover:text-primary-500 disabled:cursor-not-allowed`}
+      className={`flex gap-2 data-[outline=true]:border-[1px] data-[outline=true]:text-primary-500 data-[outline=true]:border-primary-500 data-[outline=true]:hover:bg-primary-500 data-[outline=true]:hover:text-secondary-500 bg-primary-500 text-secondary-500 [&:not(:disabled)]:hover:bg-primary-400 items-center justify-center w-full py-2 max-h-[40px] data-[outline=true]:bg-transparent transition rounded-md text-lg disabled:brightness-75 data-[outline=true]:disabled:hover:bg-transparent data-[outline=true]:disabled:brightness-90 data-[outline=true]:disabled:hover:text-primary-500 disabled:cursor-not-allowed`}
       onClick={onClick}
     >
       {Icon && !loading && <Icon />}
