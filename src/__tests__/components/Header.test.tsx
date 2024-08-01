3483091058ef0333d569e8c72e243245baa0b2b7
Felipe Souza <felipesouza036@gmail.com>
2024-08-01 13:10:09 -0300

    test (Header): Add header unit tests

--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -3,11 +3,25 @@ import "@testing-library/jest-dom";
 import Header from "@/app/components/Header";
 
 describe("<Header />", () => {
-  it("should render the add category button", async () => {
-    const { findByRole } = render(<Header />);
+  it("should render the header", () => {
+    const { getByPlaceholderText } = render(
+      <Header
+        onSearch={(text: string) => null}
+        user={{ name: "test", id: "test", photo: "https://test" }}
+      />
+    );
 
-    const header = (await findByRole("heading")) as HTMLButtonElement;
+    const header = getByPlaceholderText(/buscar/i);
 
-    expect(header).toBeInTheDocument();
+    expect(header).toBeVisible();
+  });
+  it("should render a skeleton to the user when not loaded yet", () => {
+    const { getByPlaceholderText } = render(
+      <Header onSearch={(text: string) => null} />
+    );
+
+    const header = getByPlaceholderText(/buscar/i);
+
+    expect(header).toBeVisible();
   });
 });
