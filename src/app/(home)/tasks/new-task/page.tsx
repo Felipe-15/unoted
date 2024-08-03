617672aac7bdb44e3ec0ad5927835a97440ebff3
Felipe Souza <felipesouza036@gmail.com>
2024-08-03 11:31:00 -0300

    feat (NewTask): Add animation transitions

--- src/app/(home)/tasks/new-task/page.tsx
+++ src/app/(home)/tasks/new-task/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 import usePage from "./usePage";
-import { toast, Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import Link from "next/link";
 import { openSans } from "@/app/fonts";
+
+import { AnimatePresence, motion } from "framer-motion";
 
 import StandardPage from "@/app/components/StandardPage";
 import CategoryDropdown from "@/app/components/CategoryDropdown";
 import Button from "@/app/components/Button";
 
-import { BsArrowLeft, BsFillTrashFill } from "react-icons/bs";
-import { BiCalendar, BiHelpCircle } from "react-icons/bi";
+import { BsArrowLeft } from "react-icons/bs";
+import { BiHelpCircle } from "react-icons/bi";
 import { MdOutlineMoreTime } from "react-icons/md";
 import Task from "@/app/components/Task";
 import DateInput from "@/app/components/DateInput";
@@ -37,7 +39,7 @@ const NewTaskPage = () => {
   return (
     <StandardPage headerType="noSearch" user={user}>
       <Toaster />
-      <div className="flex justify-between items-center">
+      <header className="flex justify-between items-center">
         <Link
           href="/tasks "
           className="flex gap-4 text-primary-500 transition hover:text-primary-400 text-xl items-center"
@@ -58,36 +60,44 @@ const NewTaskPage = () => {
           />
           <DateInput date={date} onSelectDate={handleSelectDate} />
         </ResponsiveHolder>
-      </div>
+      </header>
       <div className="flex flex-col gap-3 justify-start items-center md:-ml-10 pt-12 max-h-8/10 overflow-y-auto my-2">
-        <div className="flex flex-col w-fit items-start gap-4">
-          {tasks?.map((task) => (
-            <div
-              key={task.id.toString()}
-              className="flex gap-2 items-center justify-center group sm:max-w-[95%]"
+        <ul className="flex flex-col w-fit items-start gap-4">
+          <AnimatePresence>
+            {tasks?.map((task) => (
+              <motion.li
+                layout
+                initial={{ x: 100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: -100, opacity: 0 }}
+                transition={{ type: "ease" }}
+                key={task.id.toString()}
+                className="flex gap-2 items-center justify-center group sm:max-w-[95%]"
+              >
+                <Task
+                  editable
+                  text={task.text}
+                  checked={task.checked}
+                  onEdit={(text) => handleEditTask(text, task.id)}
+                  onToggleCheck={(checked) => handleCheckTask(checked, task.id)}
+                  onDelete={() => handleDeleteTask(task.id)}
+                />
+              </motion.li>
+            ))}
+            <motion.button
+              layout
+              onClick={handleAddTask}
+              className="flex gap-2 text-gray-500 text-semibold w-full cursor-pointer"
             >
-              <Task
-                editable
-                text={task.text}
-                checked={task.checked}
-                onEdit={(text) => handleEditTask(text, task.id)}
-                onToggleCheck={(checked) => handleCheckTask(checked, task.id)}
-                onDelete={() => handleDeleteTask(task.id)}
-              />
-            </div>
-          ))}
-          <button
-            onClick={handleAddTask}
-            className="flex gap-2 text-gray-500 text-semibold w-full cursor-pointer"
-          >
-            <span className="h-[24px] w-[24px] rounded-sm border-[2px] border-gray-500 shrink-0"></span>
-            <p className={openSans.className}>
-              Clique aqui para adicionar uma tarefa...
-            </p>
-          </button>
-        </div>
+              <span className="h-[24px] w-[24px] rounded-sm border-[2px] border-gray-500 shrink-0"></span>
+              <p className={openSans.className}>
+                Clique aqui para adicionar uma tarefa...
+              </p>
+            </motion.button>
+          </AnimatePresence>
+        </ul>
       </div>
-      <div className="flex justify-between mt-auto w-full items-center">
+      <footer className="flex justify-between mt-auto w-full items-center">
         <button className="p-1 text-primary-500/70 transition hover:text-primary-500 text-2xl">
           <BiHelpCircle />
         </button>
@@ -99,7 +109,7 @@ const NewTaskPage = () => {
             onClick={handleCreateTasks}
           />
         </div>
-      </div>
+      </footer>
     </StandardPage>
   );
 };
